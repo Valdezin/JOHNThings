6ec6aeebd1608b03e514403491b6d418c545d0f2
VALDEZ <VALDEZ@VALDEZ>
2022-08-07 18:50:32 +0800

    Add project files.

---
+++ JOHNThings/archives_webview.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Webkit;
+using AndroidX.AppCompat.App;
+using Android.Views;
+using JOHNThings;
+
+namespace archives_website
+{
+    [Activity(Label = "archives"  , Theme = "@style/AppTheme.NoActionBar", MainLauncher = false)]
+    public class archives_webview : AppCompatActivity
+    {
+        WebView webView;
+
+        public override bool OnKeyDown(Android.Views.Keycode keyCode, Android.Views.KeyEvent e)
+        {
+            if (keyCode == Keycode.Back && webView.CanGoBack())
+            {
+                webView.GoBack();
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.archives);
+            webView = FindViewById<WebView>(Resource.Id.webView);
+            webView.Settings.JavaScriptEnabled = true;
+            webView.Settings.DomStorageEnabled = true;
+            webView.SetWebViewClient(new WebViewClient());
+
+            webView.LoadUrl("https://archive.vldz.ga");
+
+            webView.SetWebViewClient(new WebViewClient()
+            {
+
+            });
+
+
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+    }
+}
